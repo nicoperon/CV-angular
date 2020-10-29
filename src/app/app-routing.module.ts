443c4f261dfdc5cca93b247f91b5fb8d20b1a2e1
nicoperon <nicolas.peron@ynov.com>
2020-10-30 00:23:09 +0100

    projet-cv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './inc/not-found/not-found.component';
+import { CvComponent } from './cv/cv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: IndexComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'cv', component: CvComponent},
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './inc/header/header.component';
+import { FooterComponent } from './inc/footer/footer.component';
+import { NotFoundComponent } from './inc/not-found/not-found.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { CvComponent } from './cv/cv.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    IndexComponent,
+    HomeComponent,
+    CvComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
